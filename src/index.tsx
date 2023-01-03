7a9e18cff15887de2a49aa9168d4b075181f14f8
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-01-04 01:10:14 +0200

    feat: create input component

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import { ChangeEvent, FunctionComponent, useState } from "react";
+import { scriptMask } from "./scripts";
+import { inputMaskPropsType } from "./types";
+
+type eventChangeType = ChangeEvent<HTMLInputElement>;
+
+export const InputMask: FunctionComponent<inputMaskPropsType> = ({ mask, type, onChange, ...rest }) => {
+  const [inputMask, SetInputMask] = useState("");
+
+  function handler(event: eventChangeType) {
+    if (type === "number") {
+      const numbers = RegExp(/[À-ü]|[A-Za-z]/g).test(event.target.value);
+      if (!numbers) {
+        SetInputMask(scriptMask({ value: event.target.value, pattern: mask }));
+      }
+    } else {
+      SetInputMask(scriptMask({ value: event.target.value, pattern: mask }));
+    }
+    event.target.value = event.target.value.slice(0, mask.length);
+
+    if (onChange) {
+      onChange(event);
+    }
+  }
+
+  return <input type="text" {...rest} value={inputMask} onChange={handler} />;
+};
