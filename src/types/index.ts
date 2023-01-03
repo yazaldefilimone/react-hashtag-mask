3ae8afd5f816907453820fd7717434fc3a918064
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-01-04 01:10:01 +0200

    feat: create type of input type

---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export interface inputMaskPropsType
+  extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+  mask: string;
+  type?: "number" | "text";
+}
