d7bef03f3883f9db5472c646e71fcfaa84a629a6
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-01-04 20:25:43 +0200

    chore: config run code using `vitest`

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    setupFiles: './tests/shared/setup.js',
+  },
+});
